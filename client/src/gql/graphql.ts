550726aa75338a8c2dca5ca3ef08b22d2df237b2
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-12-09 18:44:29 +0900

    クライアント用にGraphQLのコード生成をできるようにした

---
+++ client/src/gql/graphql.ts
@@ -0,0 +1,59 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  Time: { input: any; output: any; }
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createPaste: Paste;
+};
+
+
+export type MutationCreatePasteArgs = {
+  input: NewPaste;
+};
+
+export type NewPaste = {
+  content: Scalars['String']['input'];
+  language?: InputMaybe<Scalars['String']['input']>;
+  title: Scalars['String']['input'];
+};
+
+export type Paste = {
+  __typename?: 'Paste';
+  author: Scalars['String']['output'];
+  content: Scalars['String']['output'];
+  createdAt: Scalars['Time']['output'];
+  id: Scalars['ID']['output'];
+  language?: Maybe<Scalars['String']['output']>;
+  title: Scalars['String']['output'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  accessUser: User;
+  paste: Paste;
+};
+
+
+export type QueryPasteArgs = {
+  id: Scalars['ID']['input'];
+};
+
+export type User = {
+  __typename?: 'User';
+  traPId: Scalars['ID']['output'];
+};
